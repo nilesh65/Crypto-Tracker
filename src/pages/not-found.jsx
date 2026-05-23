6173a4f6ab69c0fd03539aa3e55518500c508c5e
niles <nileshmkjee123@gmail.com>
2026-05-23 23:32:19 +0530

    re created the not-found page design

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -1,35 +1,14 @@
 import { Link } from "react-router";
 
-Link
 const NotFoundPage = () => {
-    return ( <div style={styles.container}>
-        <h1 style={styles.title}>404</h1>
-        <p style={styles.message}>
-        Oops! The page you're looking for does not exist
-        </p>
-        <Link to='/' style={styles. link}>
-           ⬅️ Go Back Home
-        </Link>
-    </div> );
-}
- const styles = {
-    container: {
-textAlign: 'center',
-padding: '80px 20px',
-color: '#fff'
-},
-title: {
-fontSize: '72px',
-marginBottom: '20px'
-},
-message: {
-fontSize: '18px',
-marginBottom: '30px'
-},
-link:{
-textDecoration: 'none',
-color: '#007bff',
-fontWeight: 'bold'
-}
- }
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <p>Oops! The page you are looking for does not exist.</p>
+
+      <Link to="/">⬅ Back Home</Link>
+    </div>
+  );
+};
+
 export default NotFoundPage;
