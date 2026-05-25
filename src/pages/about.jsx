d56a8222d28771d07db445fc4e94405842eed66d
niles <nileshmkjee123@gmail.com>
2026-05-25 19:02:41 +0530

    Improved the about section

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,16 +1,20 @@
 const AboutPage = () => {
-    return ( <div className="about">
-        <h1>About Crypto Tracker</h1>
-        <p>
-        Crypto Tracker is a simple React application that displays live
-        cryptocurrency data using the CoinGecko API.
-      </p>
+  return (
+    <div className="about">
+      <h1>About Crypto Tracker</h1>
+
       <p>
-        You can explore the top cryptocurrencies by market cap, filter by name
-        or symbol, and sort them by price, market cap, or 24-hour change.
+        Crypto Tracker is a React application that displays live
+        cryptocurrency data powered by CoinGecko API.
       </p>
 
-    </div> );
-}
+      <p>
+        You can explore the top cryptocurrencies by market cap, filter by name
+        or symbol, and sort them by price, market cap, or 24-hour change, create your own watchlist and 
+        get a detailed view of a particular coin.
+      </p>
+    </div>
+  );
+};
 
 export default AboutPage;
