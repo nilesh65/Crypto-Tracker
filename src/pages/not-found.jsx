fbdfb1e57a8e8a34358bba9b8ad25b6375425a14
niles <nileshmkjee123@gmail.com>
2026-05-25 19:04:11 +0530

    Updated the not found page

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router";
 
 const NotFoundPage = () => {
   return (
-    <div className="not-found">
+    <main className="not-found">
       <h1>404</h1>
       <p>Oops! The page you are looking for does not exist.</p>
 
       <Link to="/">⬅ Back Home</Link>
-    </div>
+    </main>
   );
 };
 
