4c4da662ccaf1cd64f29be1d0c28d6b357e6adf9
niles <nileshmkjee123@gmail.com>
2026-05-25 18:54:32 +0530

    Removed the option of selecting 5 coins min 10 starting value

--- src/components/LimitSelector.jsx
+++ src/components/LimitSelector.jsx
@@ -1,14 +1,20 @@
-const LimitSelector = ({limit, onLimitChange}) => {
-    return (  <div className="controls">
-      <label htmlFor="limit">Show: </label>
-      <select value={limit} id="limit" onChange={(e) => onLimitChange(Number(e.target.value))}>
-        {/* <option value="5">5</option> */}
+const LimitSelector = ({ limit = 10, onLimitChange }) => {
+  return (
+    <div className="controls">
+      <label htmlFor="limit">Show:</label>
+
+      <select
+        value={limit}
+        id="limit"
+        onChange={(e) => onLimitChange(Number(e.target.value))}
+      >
         <option value="10">10</option>
         <option value="20">20</option>
         <option value="50">50</option>
         <option value="100">100</option>
       </select>
-    </div>);
-}
+    </div>
+  );
+};
 
 export default LimitSelector;
